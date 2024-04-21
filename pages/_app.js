c5ad405f29ae1d46da673503a846c6691a3ba2d1
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-21 11:16:14 +0530

    initialization of project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>jadoo aagaya</title>
+        <link rel="icon" href="/favicon-16x16.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
